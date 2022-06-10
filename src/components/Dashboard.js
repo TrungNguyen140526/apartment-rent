bca4f24f754bb000bfe30e29ab9f0f8cf0f8490c
TrungNguyen140526 <90686741+TrungNguyen140526@users.noreply.github.com>
2022-06-10 10:37:52 +0700

    Firebase Update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 export default function Dashboard() {
 
