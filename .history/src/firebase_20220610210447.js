57f2298347f9db4f7ee206fa61bff9acf121c21f
tthanhtung92 <55181588+tthanhtung92@users.noreply.github.com>
2022-06-10 21:05:16 +0700

    test sửa env

--- .history/src/firebase_20220610210447.js
+++ .history/src/firebase_20220610210447.js
@@ -6,9 +6,9 @@ const app = firebase.initializeApp({
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKE,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_APP_ID
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
+    measurementId: REACT_APP_FIREBASE_APP_ID
 })
 
 export const auth = app.auth()
