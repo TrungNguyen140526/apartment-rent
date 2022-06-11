4ad8c40188d016ebc9455a49fb17de6157bc409e
TrungNguyen140526 <90686741+TrungNguyen140526@users.noreply.github.com>
2022-06-11 22:58:53 +0700

    Search and Suggestion Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,8 @@
-import { Card, Button } from "react-bootstrap"
+import { Card, Button, Container } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { auth } from "../firebase"
-
+import React from "react"
 
 
 export default function Login() {
@@ -23,12 +23,18 @@ export default function Login() {
 
     return (
         <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Log In</h2>
-                    <Button onClick={signInWithGoogle} className="w-100 mt-3">Sign In With Google</Button>
-                </Card.Body>
-            </Card>
+            <Container
+                className="d-flex align-items-center justify-content-center"
+                style={{ minHeight: "100vh" }}>
+                <div className="w-100" style={{ maxWidth: "400px " }}>
+                    <Card>
+                        <Card.Body>
+                            <h2 className="text-center mb-4">Log In</h2>
+                            <Button onClick={signInWithGoogle} className="w-100 mt-3">Sign In With Google</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </Container>
         </>
     )
 }
